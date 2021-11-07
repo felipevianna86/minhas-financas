b3667f58359751b96597bbe4f599d02e89309798
felipevianna86 <felipe.vianna86@gmail.com>
2021-11-07 19:17:58 -0300

    #12 - Atualizando pra Junit 5.

--- Downloads/minhas-financas/src/test/java/com/felipe/minhasfinancas/usuario/repository/UsuarioRepositoryTest.java
+++ Downloads/minhas-financas/src/test/java/com/felipe/minhasfinancas/usuario/repository/UsuarioRepositoryTest.java
@@ -2,14 +2,14 @@ package com.felipe.minhasfinancas.usuario.repository;
 
 import com.felipe.minhasfinancas.usuario.model.Usuario;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 public class UsuarioRepositoryTest {
 
     @Autowired
