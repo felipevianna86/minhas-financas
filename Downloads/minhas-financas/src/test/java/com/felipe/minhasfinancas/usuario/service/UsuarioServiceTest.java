b42a54986c05f74c36b57c6ef0a3719e7e1d33c6
felipevianna86 <felipe.vianna86@gmail.com>
2021-11-28 20:22:59 -0300

    #20 - Inserindo Mockito no projeto e ajustando UsuarioServiceTest.

--- Downloads/minhas-financas/src/test/java/com/felipe/minhasfinancas/usuario/service/UsuarioServiceTest.java
+++ Downloads/minhas-financas/src/test/java/com/felipe/minhasfinancas/usuario/service/UsuarioServiceTest.java
@@ -3,11 +3,15 @@ package com.felipe.minhasfinancas.usuario.service;
 import com.felipe.minhasfinancas.exceptions.RegraNegocioException;
 import com.felipe.minhasfinancas.usuario.model.Usuario;
 import com.felipe.minhasfinancas.usuario.repository.UsuarioRepository;
+import com.felipe.minhasfinancas.usuario.service.impl.UsuarioServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,26 +20,30 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-    @Autowired
     UsuarioService usuarioService;
 
-    @Autowired
+    @MockBean
     UsuarioRepository usuarioRepository;
 
+    @BeforeEach
+    public void setUp(){
+        this.usuarioService = new UsuarioServiceImpl(this.usuarioRepository);
+    }
+
     @Test()
-    public void deveValidarEmail(){
-        createUser();
+    public void deveValidarEmailUsuarioNaoExistente(){
+        Mockito.when(this.usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
+
+        this.usuarioService.validarEmail("felipe@email.com");
+    }
+
+    @Test()
+    public void deveValidarEmailUsuarioExistente(){
+        Mockito.when(this.usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
         Assertions.assertThrows(RegraNegocioException.class, () ->{
             usuarioService.validarEmail("felipe.vianna86@gmail.com");
         });
     }
 
-    private Usuario createUser(){
-        Usuario usuario = Usuario.builder()
-                .nome("Felipe Vianna")
-                .email("felipe.vianna86@gmail.com")
-                .build();
-        return usuarioRepository.save(usuario);
-    }
 }
