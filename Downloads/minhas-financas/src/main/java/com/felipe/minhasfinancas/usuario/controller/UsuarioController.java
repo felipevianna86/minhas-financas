3e0a41148e287c61efd9fccfffaba7a1727b5a91
felipevianna86 <felipe.vianna86@gmail.com>
2021-12-23 11:28:13 -0300

    #25 - Criando endpoint pra autenticar usuário.

--- Downloads/minhas-financas/src/main/java/com/felipe/minhasfinancas/usuario/controller/UsuarioController.java
+++ Downloads/minhas-financas/src/main/java/com/felipe/minhasfinancas/usuario/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.felipe.minhasfinancas.usuario.controller;
 
+import com.felipe.minhasfinancas.exceptions.ErroAutenticacaoException;
 import com.felipe.minhasfinancas.exceptions.RegraNegocioException;
 import com.felipe.minhasfinancas.usuario.dto.UsuarioDTO;
 import com.felipe.minhasfinancas.usuario.model.Usuario;
@@ -23,6 +24,16 @@ public class UsuarioController {
             Usuario usuario = this.usuarioService.salvarUsuario(usuarioDTO);
             return new ResponseEntity(usuario, HttpStatus.CREATED);
         }catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/autenticar")
+    public ResponseEntity autenticar(@RequestBody UsuarioDTO usuarioDTO){
+        try{
+            Usuario usuarioAutenticado = usuarioService.autenticar(usuarioDTO.getEmail(), usuarioDTO.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        }catch (ErroAutenticacaoException e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
